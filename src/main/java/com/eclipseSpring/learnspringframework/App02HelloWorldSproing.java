cc0a731a8905de78db73d03ee1f9113b4ba9761b
ParkJoonha <parkaso1@gmail.com>
2023-07-06 00:37:53 +0900

    다양한 빈 연결 연습

--- src/main/java/com/eclipseSpring/learnspringframework/App02HelloWorldSproing.java
+++ src/main/java/com/eclipseSpring/learnspringframework/App02HelloWorldSproing.java
@@ -3,6 +3,9 @@ package com.eclipseSpring.learnspringframework;
 import org.springframework.boot.autoconfigure.web.reactive.HttpHandlerAutoConfiguration.AnnotationConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class App02HelloWorldSproing {
 
 	public static void main(String[] args) {
@@ -14,9 +17,18 @@ public class App02HelloWorldSproing {
 		System.out.println(context.getBean("person"));
 		System.out.println(context.getBean("person2MethodCall"));
 		System.out.println(context.getBean("person3Parameters"));
-		System.out.println(context.getBean("address"));
-		// Address 클레스로 구현된 함수가 여러개 빈에 등록되있다면 에러가 난다.
+		System.out.println(context.getBean("address2"));
+		// Address 클레스로 구현된 함수가 여러 개 생성되어 빈에 등록되있다면 에러가 난다.
+		// 1. 하나의 빈을 @Primary 어노테이션을 통해 기본값으로 설정
 		System.out.println(context.getBean(Address.class));
+		System.out.println(context.getBean(Person.class));
+		// 2. @Qualifire를 설정 후 다른 파라미터 내의 @Qualifire("이름")을 통해 Bean 간의 연결을 시켜준다.
+		System.out.println(context.getBean("person5Qualifire"));
+
+		//System.out.println
+		Arrays.stream(context.getBeanDefinitionNames())
+				.forEach(System.out::println);
+									//메서드 참조
 
 	}
 
--- src/main/java/com/eclipseSpring/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/eclipseSpring/learnspringframework/HelloWorldConfiguration.java
@@ -1,7 +1,9 @@
 package com.eclipseSpring.learnspringframework;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 record Person (String name, int age, Address address) { };
 record Address (String city, String firstLine) { };
@@ -32,17 +34,30 @@ public class HelloWorldConfiguration {
 
     // 파라미터 구현
     @Bean
-    public Person person3Parameters(String name, int age, Address address) {
+    public Person person3Parameters(String name, int age, Address address2) {
+        return new Person(name, age, address2);
+    }
+
+    @Bean
+    @Primary
+    public Person person4Parameters(String name, int age, Address address) {
         return new Person(name, age, address);
     }
 
-    @Bean (name = "address2")
-    public Address address() {
-        return new Address("Seong-nam", "Bun-dang");
+    @Bean
+    public Person person5Qualifire(String name, int age, @Qualifier("address2qualifire") Address address) {
+        return new Person(name, age, address);
     }
 
-    @Bean (name = "address3")
-    public Address address3() {
-        return new Address("Busan", "Bun-dang");
+    @Bean (name = "address1")
+    @Primary
+    public Address address() {
+        return new Address("Seongnam", "Bundang");
+    }
+
+    @Bean (name = "address2")
+    @Qualifier("address2qualifire")
+    public Address address2() {
+        return new Address("Deajeun", "Junggu");
     }
 }
